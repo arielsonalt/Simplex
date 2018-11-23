9e61872de5b78fa49ebe8dbdc2b2b04cbd1b961d
arielsonalt <32400603+arielsonalt@users.noreply.github.com>
2018-11-23 18:17:38 -0200

    Atualização

--- src/Simplex.cpp
+++ src/Simplex.cpp
@@ -35,43 +35,106 @@ void Simplex::simplexSimples()
 
     double colunaResult[quantRestricoes+1];
     int quantLinhaTabela = problema.getFuncaoObjetivoPronto().getVariaveis().size()+1;
-    double tabela[quantLinhaTabela][quantRestricoes*2];
+    int colunaTabela = quantRestricoes + problema.getFuncaoObjetivoPronto().getVariaveis().size();
+    double tabela[quantLinhaTabela][colunaTabela];
 
     for (int j = 0; j < quantRestricoes+1; j++)
+    {
+        if(j >= quantRestricoes)
         {
-            if(j >= quantRestricoes){
-                if(j == quantRestricoes){
-                    colunaEsq[j]="Z";
-                }
-            }else{
-                colunaEsq[j] = problema.getRestricao(j)->getFolga().getNome();
-                //cout<<problema.getRestricao(j)->getFolga().getNome()<<endl;
+            if(j == quantRestricoes)
+            {
+                colunaEsq[j]="Z";
             }
-            //cout<<colunaEsq[j]<<endl;
         }
-
-    linhaTop[(problema.getFuncaoObjetivoPronto().getVariaveis().size()*2)]="B";
-    for(int k=0; k< sizeof(linhaTop)/sizeof(int);k++){
-
+        else
+        {
+            colunaEsq[j] = problema.getRestricao(j)->getFolga().getNome();
+            //cout<<problema.getRestricao(j)->getFolga().getNome()<<endl;
+        }
+        //cout<<colunaEsq[j]<<endl;
     }
 
-    for(unsigned int i = 0; i<quantLinhaTabela; i++){
-        for(unsigned int j = 0; j<quantRestricoes*2;j++){
+    linhaTop[(problema.getFuncaoObjetivoPronto().getVariaveis().size()*2)]="B";
+
+    int l=0;
+    for(int k=0; k < problema.getFuncaoObjetivoPronto().getVariaveis().size(); k++)
+    {
+        linhaTop[k] = problema.getFuncaoObjetivoPronto().getVariavel(k).getNome();
+        l++;
+    }
+
+
+    for(int m=0; m< problema.getRestricoes().size(); m++)
+    {
+        linhaTop[l] = problema.getRestricao(m)->getFolga().getNome();
+        l++;
+    }
+
+    for(int k=0; k < sizeof(linhaTop)/sizeof(int) ; k++)
+    {
+        //cout<<linhaTop[k]<<endl;
+    }
+
+
+
+
+
+    for(unsigned int i = 0; i<quantLinhaTabela; i++)
+    {
+        for(unsigned int j = 0; j<colunaTabela; j++)
+        {
             tabela[i][j] = 0;
-            cout<<tabela[i][j]<<" ";
+            //cout<<tabela[i][j]<<" ";
         }
-        cout<<""<<endl;
+        //cout<<""<<endl;
     }
     cout<<sizeof(linhaTop)/sizeof(int)<<endl;
 
-    for(unsigned int i = 0; i<quantLinhaTabela; i++){
-        for(unsigned int j = 0; j<quantRestricoes*2;j++){
+    for(unsigned int i = 0; i<quantLinhaTabela-1; i++)
+    {
+        for(unsigned int j = 0; j<colunaTabela; j++)
+        {
+            if(j<problema.getRestricoes().size())
+            {
+                for(int k=0; k < problema.getRestricao(i)->getVariaveis().size(); k++)
+                {
+                    if(problema.getRestricao(i)->getVariavel(k)->getNome() ==  linhaTop[j])
+                    {
 
+                        tabela[i][j] = problema.getRestricao(i)->getVariavel(k)->getValor();
+                    }
+                    else
+                    {
+
+                        tabela[i][j] = 0;
+                    }
+
+                }
+
+            }
+            else
+            {
+                if(problema.getRestricao(i)->getFolga().getNome() == linhaTop[j])
+                {
+
+                    tabela[i][j] = problema.getRestricao(i)->getFolga().getValor();
+
+                }
+                else
+                {
+                    tabela[i][j] = 0;
+
+                }
+            }
         }
-
+cout<<"oi"<<endl;
     }
-    for(unsigned int i = 0; i<quantLinhaTabela; i++){
-        for(unsigned int j = 0; j<quantRestricoes*2;j++){
+
+    for(unsigned int i = 0; i<quantLinhaTabela; i++)
+    {
+        for(unsigned int j = 0; j<colunaTabela; j++)
+        {
             cout<<tabela[i][j]<<" ";
         }
         cout<<""<<endl;
